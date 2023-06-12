dcd37b7edcf1304e7b77d919b1c0c33fa19733d2
jememj <laniragma@gmail.com>
2023-06-12 23:01:09 +0300

    fix

---
+++ config/env/production/database.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 1337),
+    },
+  },
+});
